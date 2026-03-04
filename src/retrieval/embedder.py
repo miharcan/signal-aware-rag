5a579d6ab8a1a27c4b74e2ec7bbdceb1c3f7ed2e
miharc <mihael.arcan@gmail.com>
2026-03-04 10:45:38 +0000

    add embedding + retrieval layer

---
+++ src/retrieval/embedder.py
@@ -0,0 +1,9 @@
+from sentence_transformers import SentenceTransformer
+import numpy as np
+
+class Embedder:
+    def __init__(self, model_name="sentence-transformers/all-MiniLM-L6-v2"):
+        self.model = SentenceTransformer(model_name)
+
+    def embed(self, texts):
+        return np.array(self.model.encode(texts, show_progress_bar=False))
